ca9eec852b3b496d781821da629625a64b1a8d0d
Benson <bensonmakau2000@gmail.com>
2022-07-27 11:49:34 +0300

    commonjs module library

--- script.js
+++ script.js
@@ -48,28 +48,37 @@
 // console.log(lastPost);
 
 //The Module Pattern
-const ShoppingCart2 = (function () {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
 
-  const addToCart = function (product, quantity) {
-    cart.push(product, quantity);
-    console.log(`${quantity} ${product} added to cart`);
-  };
+//   const addToCart = function (product, quantity) {
+//     cart.push(product, quantity);
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
 
-  const orderStock = function (product, quantity) {
-    console.log(`${quantity} ${product} ordered from supplier`);
-  };
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   };
 
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity,
-  };
-})();
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
 
-ShoppingCart2.addToCart('apple', 4);
-ShoppingCart2.addToCart('pizza', 2);
+// ShoppingCart2.addToCart('apple', 4);
+// ShoppingCart2.addToCart('pizza', 2);
+
+//common js modules
+//export
+// export.addToCart = function (product, quantity) {
+//     cart.push(product, quantity);
+//     console.log(`${quantity} ${product} added to cart`);
+// };
+// //import
+// const {addToCart} = require('./shoppingCart.js')
